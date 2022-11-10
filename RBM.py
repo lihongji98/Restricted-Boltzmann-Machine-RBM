4f6c6d8e6e6bb00ba8e01193b2aa2b41d5d0d83d
Lihong Ji <294672570@qq.com>
2022-11-10 16:43:44 +0100

    1

--- RBM.py
+++ RBM.py
@@ -1,15 +1,21 @@
 import numpy as np
 import itertools
+import time
+import matplotlib.pyplot as plt
+
+
 
 class RBM:
-    def __init__(self, v_dim, h_dim, lr=10e-2, gibbs_num=10):
+    def __init__(self, v_dim, h_dim, lr=10e-2, gibbs_num=15):
         self.v_dim = v_dim
         self.h_dim = h_dim
         self.lr = lr
-        self.v_bias = np.random.normal(size = (1,self.v_dim))
-        self.h_bias = np.random.normal(size =(1,self.h_dim))
+        self.v_bias = np.random.normal(0, 0.01, size = (1,self.v_dim))
+        self.h_bias = np.random.normal(0, 0.01, size =(1,self.h_dim))
         self.W = np.random.normal(size = (self.v_dim, self.h_dim))
         self.gibbs_num = gibbs_num
+        self.v_w, self.v_v, self.v_h = 0, 0, 0
+        self.momentum = 0.9
 
     
     # when a input vector V is recieved, p(h=1|v) can be computed
@@ -50,10 +56,10 @@ class RBM:
             state_h, _ = self.sample_h(v)
             state_v, _ = self.sample_v(state_h)
             i += 1
-            state_v[v0 < 0] = v_0[v0 < 0]
+            #state_v[v0 < 0] = v_0[v0 < 0]
         else: 
             v_k = state_v
-            v_k[v0 < 0] = v_0[v0 < 0]
+            #v_k[v0 < 0] = v_0[v0 < 0]
             _, p_hk_v = self.sample_h(v_k)
         
         return v_0, v_k, p_h0_v, p_hk_v
@@ -65,58 +71,124 @@ class RBM:
         dh_bias = np.sum(p_h0_v - p_hk_v)
         dv_bias = np.sum(v_0 - v_k)
         
-        self.W += self.lr * dw
-        self.v_bias += self.lr * dv_bias
-        self.h_bias += self.lr * dh_bias  
+        self.v_w = self.momentum * self.v_w + (1 - self.momentum) * dw
+        self.v_h = self.momentum * self.v_h + (1 - self.momentum) * dh_bias
+        self.v_v = self.momentum * self.v_v + (1 - self.momentum) * dv_bias 
+
+        self.W += self.lr * self.v_w
+        self.v_bias += self.lr * self.v_v
+        self.h_bias += self.lr * self.v_h 
     # set the number of iteration and go over all the training data (binary vectors) for one epoch.
 
 
-train_data = np.loadtxt(r'C:\Users\86185\Desktop\master thesis\data\Bars-and-Stripes-3x3.txt')
-rbm = RBM(train_data.shape[1], 16, lr=10e-2, gibbs_num= 10)
+def compute_px_with_Z(train_data, W, v_bias, h_bias): 
+    probability = []
+    for l in range(len(train_data)):
+        train_data_one_piece = train_data[l]
+        product_value = 1
+        exp_av = np.exp(np.dot(v_bias, train_data_one_piece))
+        for i in range(h_bias.shape[1]):
+            product_value = product_value * (np.exp(np.dot(W.T[i], train_data_one_piece) + h_bias.T[i]) + 1)
+        px_with_Z = exp_av * product_value
+        probability.append(px_with_Z[0])
+    return probability
 
-losses = [] 
 
-epochs = 1
-batch_size = 1
-for epoch in range(epochs):
-    train_loss = 0
-    epoch_loss = []
-    s = 0.
-
-    # learn parameters
-    for index in range(0, train_data.shape[0]-batch_size, batch_size):
-        v0 = train_data[index: index + batch_size]
-        vk = train_data[index: index + batch_size]
-        _, p_h0_v = rbm.sample_h(v0)
-        v0, vk, p_h0_v, p_hk_v = rbm.gibbs_sampling(v0)
-        rbm.gradient_compute(v0, vk, p_h0_v, p_hk_v)
+def compute_Z(v_dim, W, v_bias, h_bias):
+    def dic_build(dic_candidate, dim):
+        i= 0
+        for item in itertools.product('01',repeat=dim):
+            if i not in dic_candidate:
+                dic_candidate[i] = item
+            i += 1
+        return dic_candidate
         
-        train_loss += np.mean(np.abs(v0[v0>=0] - vk[v0>=0])) # acc = 1 - train_loss
-        epoch_loss.append(train_loss/s)
-        s += 1.
+    dic_v = {}  
+    dic_v = dic_build(dic_v, v_dim)
 
-    # compute KL divergence and model likelihood
-    for index in range(0, train_data.shape[0]-batch_size, batch_size):
-        v0 = train_data[index: index + batch_size]
-        vk = train_data[index: index + batch_size]
-        _, p_h0_v = rbm.sample_h(v0)
-        v0, vk, p_h0_v, p_hk_v = rbm.gibbs_sampling(v0)
+    Z = 0
+    for l in range(len(dic_v)):
+        train_data_one = np.array(dic_v[l]).astype(int)
+        exp_av = np.exp(np.dot(v_bias, train_data_one))
+        product = 1
+        for j in range(h_bias.shape[1]):
+            product = product * (np.exp(np.dot(train_data_one.T, W.T[j]) + h_bias.T[j]) + 1)
+        total = exp_av * product
 
-        data_entropy = []   
-        data_dic = {}
-        for key in v0[0]:
-            data_dic[key] = data_dic.get(key, 0) + 1
+        Z += total
+    return Z
 
-        print(v0, data_dic.values())
+
+
+if __name__ == '__main__':
+    train_data = np.loadtxt('./Bars-and-Stripes-4x4.txt')
+
+    visible_node_num = train_data.shape[1]
+    hidden_node_num = 57
+    lr = 5*10e-3
+    gibbs_num = 20
+
+    epochs = 2000
+    batch_size = 1
+
+    rbm = RBM(visible_node_num, hidden_node_num, lr, gibbs_num)
+
+    KL_list = []
+    log_LKH_list = []
+
+    for epoch in range(epochs):
+        epoch_start_time = time.time()
+        for index in range(0, train_data.shape[0]-batch_size, batch_size):
+            v0 = train_data[index: index + batch_size]
+            vk = train_data[index: index + batch_size]
+            _, p_h0_v = rbm.sample_h(v0)
+            v0, vk, p_h0_v, p_hk_v = rbm.gibbs_sampling(v0)
+            rbm.gradient_compute(v0, vk, p_h0_v, p_hk_v)
         
-    print('====')
+
+        Z = compute_Z(train_data.shape[1], rbm.W, rbm.v_bias, rbm.h_bias) 
+        probability_list = compute_px_with_Z(train_data, rbm.W, rbm.v_bias, rbm.h_bias)
+
+        logLKH = 0
+        KL = 0
+        
+        
+        for i in range(len(probability_list)):
+            px_with_Z = probability_list[i]
+            N = len(probability_list)
+            log_lkh = np.log(px_with_Z) - np.log(Z) #####
+            logLKH += log_lkh
+
+            kl = -np.log(N)/N - np.log(px_with_Z)/N + np.log(Z)/N
+            KL += kl
+        KL /= N
+        logLKH /= N
+        KL_list.append(KL)
+        log_LKH_list.append(logLKH)
+
+        probability_list = [probability_list[i]/Z for i in range(len(probability_list))]
+        x = np.sum(probability_list)
+        
+        epoch_end_time = time.time()
+        
+        results = 'epoch:{} ==>  KL = {}, logLKH = {}, prob_sum = {:.4f}, time = {:.2f}s'.format(epoch+1, KL, logLKH, x, epoch_end_time-epoch_start_time)
+        f=open("a.txt","a")
+        f.write(results + '\n')
+        f.close()
+
+
+    kl_time = [i for i in range(len(KL_list))]
+    plt.figure(figsize=(10,5))
+    plt.plot(kl_time, KL_list)
+    plt.savefig('KL_divergence')
+
+    plt.figure(figsize=(10,5))
+    plt.plot(kl_time, log_LKH_list)
+    plt.savefig('log_Likelihood')
+
+    np.save('W.npy', rbm.W)
+    np.save('h_bias.npy', rbm.h_bias)
+    np.save('v_bias.npy', rbm.v_bias)
+
     
-
-
-    # epoch_loss --> KL (RMB & training) ⬇      likelihood ⬆
-
-
-    losses.append(epoch_loss[-1])
-    #if(epoch % 10 == 0):
-    #    print('Epoch:{0:4d} Train Loss:{1:1.4f}'.format(epoch, train_loss/s))
 
